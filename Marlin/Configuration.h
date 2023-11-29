efe8fda81c62add183fccbf23aa2e37e46afba84
Matthew Balch <matt5sean3@gmail.com>
2023-11-29 17:19:33 -0500

    Set hotend PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -667,9 +667,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    #define DEFAULT_Kp  13.83
+    #define DEFAULT_Ki   1.88
+    #define DEFAULT_Kd  25.45
   #endif
 #endif
 
