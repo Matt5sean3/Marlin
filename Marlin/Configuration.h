8cedc290816ae060ae0338514b74f3950c6b2949
Matthew Balch <matt5sean3@gmail.com>
2024-12-21 11:55:27 -0500

    Write configuration to compile to BTT SKR Mini E3 V1.2 board

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -68,7 +68,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_BTT_SKR_MINI_E3_V3_0
+  #define MOTHERBOARD BOARD_BTT_SKR_MINI_E3_V1_2
 #endif
 
 // @section serial
